d4b5a508d189cfd512aa8370e83acc3e64001fab
Craig Nielsen <craiglbn@gmail.com>
2016-12-22 09:47:48 +0200

    Finalize setup

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,3 +1,4 @@
+#!/bin/zsh
 echo "the current script assumes a debian linux environment with zsh, and pyenv virtualenv already setup"
 echo "Do you wish to continue?"
 select yn in "Yes" "No"; do
@@ -13,5 +14,11 @@ pip install -r requirements.txt;
 cd ../website-service
 npm install
 gnome-terminal -e 'webpack-dev-server --inline --hot'
-export FLASK_APP=manage.py
-flask run
+
+echo '\n\nplease enter: export FLASK_APP=manage.py, then: python -m flask run';
+exit;
+;;
+        No ) exit;;
+
+esac
+done
